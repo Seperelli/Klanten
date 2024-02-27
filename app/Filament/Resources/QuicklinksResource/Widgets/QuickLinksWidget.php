c0f12234855c0bad99efa2ecbdc347666e09561a
Seperelli <sebastian@thecornerstone.be>
2024-02-27 11:59:10 +0100

    links

---
+++ app/Filament/Resources/QuicklinksResource/Widgets/QuickLinksWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\QuicklinksResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class QuickLinksWidget extends Widget
+{
+    protected static string $view = 'filament.resources.quicklinks-resource.widgets.quick-links-widget';
+}
---
+++ resources/views/filament/resources/quicklinks-resource/widgets/quick-links-widget.blade.php
@@ -0,0 +1,12 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <div>
+            <ul>
+                <li><a href="/admin/resource1">Resource 1</a></li>
+                <li><a href="/admin/resource2">Resource 2</a></li>
+                <!-- Add more links as needed -->
+            </ul>
+        </div>
+        {{-- Widget content --}}
+    </x-filament::section>
+</x-filament-widgets::widget>
